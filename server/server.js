6789277c4b52bc9f26a802f06f4fc32228233525
Ian Smith <ianmsmith17@gmail.com>
2021-03-31 22:36:18 -0500

    project setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+app.get('/books', (req, res) => {
+  res.send({books: ['Moby Dick', 'Beowulf', 'Iliad', 'Odyssey']});
+})
